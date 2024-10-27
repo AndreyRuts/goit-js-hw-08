d1ed25dbd2b2acc5dba68ca65b5c5400fb04355f
Andrey Ruts <ravnb1989@gmail.com>
2024-10-27 14:36:25 +0200

    starter hw 8

--- js/task-1.js
+++
@@ -1,8 +0,0 @@
-const categoriesCount = document.querySelectorAll(".item")
-console.log(`Number of categories: ${categoriesCount.length}`);
-categoriesCount.forEach(element => {
-    const title = element.querySelector("h2").textContent;
-    const item = element.querySelectorAll("li").length;
-    console.log(`Category: ${title}`);
-    console.log(`Elements: ${item}`);
-});
--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-const userName = document.querySelector("#name-input")
-const greetName = document.querySelector("#name-output")
-
-userName.addEventListener("input", () => {
-    const inputValue = userName.value.trim();
-    greetName.textContent = inputValue === "" ? "Anonymous" : inputValue;
-});
